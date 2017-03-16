df7a45aef28f4ea8abc167abba1729616331f37d
dylan september <dylan september>
2017-03-16 22:39:53 +0200

    unitTestng

---
+++ src/main/java/dylan/junitTest/App.java
@@ -0,0 +1,199 @@
+package dylan.junitTest;
+
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+   private int age;
+   private String name;
+   private String surname;
+   private int day;
+   private int month;
+   private int year;
+   private Boolean pet;
+   private String petName;
+   private int oldAge;
+
+
+
+    public static void main( String[] args )
+    {
+        App a = new App();
+
+
+        Scanner input = new Scanner(System.in);
+        String dob;
+
+        System.out.println("Enter Date of Birth (DD-MM-YYYY): ");
+        a.setDayMonthYear(input.next());
+
+        System.out.println("Enter Your Name: ");
+        a.setName(input.next());
+
+        System.out.println("Enter Your Surname: ");
+        a.setSurname(input.next());
+
+        System.out.println("Do You Own A Pet?(Y/N): ");
+        a.setPet(input.next());
+
+        if(a.getPet())
+        {
+            System.out.println("Enter Your Pets Name: ");
+            a.setPetName(input.next());
+            System.out.println( "Hello World!" );
+            System.out.println( "My name is " + a.getName());
+            System.out.println( "My surname is " + a.getSurname());
+            System.out.println( "I am " + a.getAge() + " years old");
+            System.out.println( "In 30 years i will be "  + a.getOldAge() + " years old");
+            System.out.println( "My pets name is " + a.getPetName());
+            System.out.println( a.getPetName() + " ate my homework :( .........." );
+        }
+        else {
+            System.out.println("Hello World!");
+            System.out.println("My name is " + a.getName());
+            System.out.println("My surname is " + a.getSurname());
+            System.out.println("I am " + a.getAge() + " years old");
+            System.out.println("");
+            System.out.println("A long time Later....");
+            System.out.println("I have locked myself in my car for 30 years...");
+            System.out.println("I am " + a.getOldAge() + " years old now");
+            System.out.println("");
+            System.out.println("I am dead now.............");
+        }
+
+    }
+
+    public App()
+    {
+
+    }
+    public App(int age)
+    {
+        this.age = age;
+    }
+    public App(int day, int month, int year)
+    {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+
+    }
+
+    public void agePlus()
+    {
+        oldAge = age + 30;
+    }
+
+    public void setDayMonthYear(String dob)
+    {
+        String[] rs = dob.split("-");
+        day = Integer.parseInt(rs[0]);
+        month = Integer.parseInt(rs[1]);
+        year = Integer.parseInt(rs[2]);
+
+        System.out.println("date :" + day + "-" + month + "-" +year);
+    }
+
+    public void calcAge()
+    {
+
+
+
+        Calendar dob = new GregorianCalendar(year, month, day);
+        Calendar sysdate = new GregorianCalendar();
+        age = sysdate.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+        if ((dob.get(Calendar.MONTH) > sysdate.get(Calendar.MONTH))
+                || (dob.get(Calendar.MONTH) == sysdate.get(Calendar.MONTH) && dob.get(Calendar.DAY_OF_MONTH) > sysdate
+                .get(Calendar.DAY_OF_MONTH))) {
+            age--;
+        }
+        //System.out.println("" + age);
+    }
+
+
+
+
+    public void setName(String nm)
+    {
+        name = nm;
+    }
+
+    public void setSurname(String snm)
+    {
+        surname = snm;
+    }
+
+    public void setPet(String petYN)
+    {
+
+        if(petYN.toUpperCase().equals("Y"))
+        {
+
+            pet = true;
+        }
+        else if(petYN.toUpperCase().equals("N"))
+        {
+            pet = false;
+        }
+
+    }
+
+    public void setPetName(String pName)
+    {
+        petName = pName;
+    }
+
+
+
+
+
+    public int getOldAge()
+    {
+        agePlus();
+        return oldAge;
+    }
+
+    public int getAge()
+    {
+        calcAge();
+        return age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getSurname()
+    {
+        return surname;
+    }
+
+    public String getPetName()
+    {
+        return petName;
+    }
+    public Boolean getPet()
+    {
+        return pet;
+    }
+    public int getDay(){
+        return day;
+    }
+
+    public int getMonth(){
+        return month;
+
+    }
+
+    public int getYear(){
+        return year;
+    }
+}
---
+++ src/test/java/dylan/junitTest/AppTest.java
@@ -0,0 +1,96 @@
+package dylan.junitTest;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    @org.junit.Test
+    public void testAge() throws Exception
+    {
+        App app = new App(29,8,1995);
+        app.calcAge();
+        int age = app.getAge();
+
+        assertEquals(21,age);
+    }
+
+    @org.junit.Test
+    public void testSetDayMonthYear() throws Exception
+    {
+        App app = new App();
+        app.setDayMonthYear("29-08-1995");
+        assertEquals(29,app.getDay());
+        assertEquals(8,app.getMonth());
+        assertEquals(1995,app.getYear());
+    }
+
+    public void testSetGetName()
+    {
+        App app = new App();
+        app.setName("Jack");
+        assertEquals("Jack", app.getName());
+    }
+    public void testSetGetSurname()
+    {
+        App app = new App();
+        app.setSurname("Black");
+        assertEquals("Black", app.getSurname());
+    }
+    public void testSetGetPet()
+    {
+        App app = new App();
+        app.setPet("y");
+        assertTrue( app.getPet());
+        app.setPet("n");
+        assertFalse(app.getPet());
+    }
+
+    public void getOldAge()
+    {
+        App app = new App(10);
+        assertEquals(40, app.getOldAge());
+    }
+
+
+    public void testSetGetPetName()
+    {
+        App app = new App();
+        app.setPetName("Snoopy");
+        assertEquals("Snoopy", app.getPetName());
+    }
+
+
+}
